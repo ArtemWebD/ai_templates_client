da50a39c1f257af788177161cf6612abfd001af4
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:49:06 +0700

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import PageRouter from './router/PageRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AlertMessage from './components/alert/alertMessage';
 import changeTheme from './modules/color-theme/changeTheme';
 import { StoreContext } from '.';
+import Loader from './components/loader/Loader';
+import DeleteSubmitState from './modules/delete-submit/deleteSubmitState';
 
 function App() {
   const { generateTokenStore } = useContext(StoreContext);
 
   useEffect(() => {
     changeTheme();
-    generateTokenStore.getAll();
+    DeleteSubmitState.setState();
+  }, []);
+
+  useEffect(() => {
+    const setToken = async () => {
+      await generateTokenStore.getAll();
+
+      if (localStorage.getItem("generateToken")) {
+        return;
+      }
+
+      generateTokenStore.tokens.forEach((token) => {
+          if (token.count > 0) {
+              localStorage.setItem("generateToken", token.token);
+          }
+      });
+    }
+
+    setToken();
   }, []);
 
   return (
     <>
       <PageRouter />
       <AlertMessage />
+      <Loader />
     </>
   );
 }
