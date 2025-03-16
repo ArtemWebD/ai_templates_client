1ad7b58517f4b60d7853f2cbb9611e2f6c879cae
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:53:51 +0700

    added delete form

--- src/components/white-page-list/WhitePageElement.tsx
+++ src/components/white-page-list/WhitePageElement.tsx
@@ -4,6 +4,16 @@ import { ArrowUpRightSquare, Trash } from "react-bootstrap-icons";
 import { useContext } from "react";
 import { StoreContext } from "../..";
 import { IWhitePage } from "../../services/white-page/whitePageInterface";
+import { IDeleteButtonParams } from "../delete-submit-modal/deleteSubmitModalParams";
+import DeleteSubmitModal from "../delete-submit-modal/DeleteSubmitModal";
+
+const DeleteButton = ({ onClick }: IDeleteButtonParams) => {
+    return (
+        <div className="templates__delete" onClick={onClick}>
+            <Trash width={32} height={32} />
+        </div>
+    );
+}
 
 const WhitePageElement = ({ whitePage }: { whitePage: IWhitePage }) => {
     const { whitePageStore } = useContext(StoreContext);
@@ -23,9 +33,7 @@ const WhitePageElement = ({ whitePage }: { whitePage: IWhitePage }) => {
             <a href={uri} target="_blank" className="templates__link">
                 <ArrowUpRightSquare width={32} height={32} />
             </a>
-            <div className="templates__delete" onClick={removeHandler}>
-                <Trash width={32} height={32} />
-            </div>
+            <DeleteSubmitModal button={DeleteButton} callback={removeHandler} />
         </Col>
     );
 }
