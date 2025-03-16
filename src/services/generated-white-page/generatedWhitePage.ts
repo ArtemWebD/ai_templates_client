e308ccdf099df8874c2dc33cbbf5e42dc9a139b9
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:58:01 +0700

    fixed bug with request headers

--- src/services/generated-white-page/generatedWhitePage.ts
+++ src/services/generated-white-page/generatedWhitePage.ts
@@ -5,7 +5,7 @@ export default class GeneratedWhitePageService {
     static async create(id: number, prompt: string): Promise<void> {
         await API.post("/white-page/create", { id, prompt }, {
             headers: {
-                "Generate-Token": localStorage.getItem("generateToken")
+                "generate-goken": localStorage.getItem("generateToken")
             }
         });
     }
