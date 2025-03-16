212a2a003c9b0c30e731079a97767c25866cc91d
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:50:07 +0700

    added help button

--- src/components/generate-token-list/GenerateTokenList.tsx
+++ src/components/generate-token-list/GenerateTokenList.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect } from "react";
 import { Table } from "react-bootstrap";
 import { StoreContext } from "../..";
 import GenerateTokenElement from "./GenerateTokenElement";
+import HelpButton from "../help-button/HelpButton";
 
 const GenerateTokenList = ({ userId }: { userId: number }) => {
     const { generateTokenStore } = useContext(StoreContext);
@@ -16,7 +17,17 @@ const GenerateTokenList = ({ userId }: { userId: number }) => {
             <thead>
                 <tr>
                     <th>Токен</th>
-                    <th>Количество использований</th>
+                    <th>
+                        <div className="d-flex align-items-center">
+                            <div style={{ marginRight: "5px" }}>Количество использований</div>
+                            <HelpButton 
+                                text="
+                                    Количество использований токена можно изменить в таблице. Для сохранения
+                                    изменений нажмите Enter после ввода значения
+                                " 
+                            />
+                        </div>
+                    </th>
                     <th>Дата создания</th>
                 </tr>
             </thead>
