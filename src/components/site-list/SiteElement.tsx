ac52cf058933689dbe5c65d13f14b5914c979b86
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:53:22 +0700

    added pages

--- src/components/site-list/SiteElement.tsx
+++ src/components/site-list/SiteElement.tsx
@@ -4,13 +4,24 @@ import { ArrowUpRightSquare, Trash } from "react-bootstrap-icons";
 import { useContext } from "react";
 import { StoreContext } from "../..";
 import { ISite } from "../../services/site/siteInterface";
+import PageOffcanvas from "../page-offcanvas/PageOffcanvas";
+import { IDeleteButtonParams } from "../delete-submit-modal/deleteSubmitModalParams";
+import DeleteSubmitModal from "../delete-submit-modal/DeleteSubmitModal";
+
+const DeleteElement = ({ onClick }: IDeleteButtonParams) => {
+    return (
+        <div className="templates__delete" onClick={onClick}>
+            <Trash width={32} height={32} />
+        </div>
+    );
+}
 
 const SiteElement = ({ site }: { site: ISite }) => {
     const { siteStore } = useContext(StoreContext);
 
-    const { id, title, path } = site;
+    const { id, title, path, pages } = site;
     const url = new URL(path, SERVER_URL);
-    const uri = url + `?id=${id}`;
+    const uri = url + `?id=${id}&page=${pages[0]}`;
 
     const removeHandler = async () => {
         await siteStore.remove(id);
@@ -20,12 +31,14 @@ const SiteElement = ({ site }: { site: ISite }) => {
         <Col>
             <h3>{title}</h3>
             <iframe className="mt-2" src={uri} frameBorder="0" width="1280" height="1280" scrolling="no" sandbox="" />
-            <a href={uri} target="_blank" className="templates__link">
-                <ArrowUpRightSquare width={32} height={32} />
-            </a>
-            <div className="templates__delete" onClick={removeHandler}>
-                <Trash width={32} height={32} />
-            </div>
+            {
+                pages.length === 1
+                    ? <a href={uri} target="_blank" className="templates__link">
+                        <ArrowUpRightSquare width={32} height={32} />
+                      </a>
+                    : <PageOffcanvas id={id} pages={pages} path={path} />
+            }
+            <DeleteSubmitModal button={DeleteElement} callback={removeHandler} />
         </Col>
     );
 }
--- src/components/template-list/TemplateElement.tsx
+++ src/components/template-list/TemplateElement.tsx
@@ -4,13 +4,24 @@ import { SERVER_URL } from "../../constants";
 import { ArrowUpRightSquare, Trash } from "react-bootstrap-icons";
 import { useContext } from "react";
 import { StoreContext } from "../..";
+import PageOffcanvas from "../page-offcanvas/PageOffcanvas";
+import DeleteSubmitModal from "../delete-submit-modal/DeleteSubmitModal";
+import { IDeleteButtonParams } from "../delete-submit-modal/deleteSubmitModalParams";
+
+const DeleteButton = ({ onClick }: IDeleteButtonParams) => {
+    return (
+        <div className="templates__delete" onClick={onClick}>
+            <Trash width={32} height={32} />
+        </div>
+    );
+}
 
 const TemplateElement = ({ template }: { template: ITemplate }) => {
     const { templateStore } = useContext(StoreContext);
 
-    const { id, title, path } = template;
+    const { id, title, path, pages } = template;
     const url = new URL(path, SERVER_URL);
-    const uri = url + `?id=${id}`;
+    const uri = url + `?id=${id}&page=${pages[0]}`;
 
     const removeHandler = async () => {
         await templateStore.remove(id);
@@ -20,12 +31,14 @@ const TemplateElement = ({ template }: { template: ITemplate }) => {
         <Col>
             <h3>{title}</h3>
             <iframe className="mt-2" src={uri} frameBorder="0" width="1280" height="1280" scrolling="no" sandbox="" />
-            <a href={uri} target="_blank" className="templates__link">
-                <ArrowUpRightSquare width={32} height={32} />
-            </a>
-            <div className="templates__delete" onClick={removeHandler}>
-                <Trash width={32} height={32} />
-            </div>
+            {
+                pages.length === 1
+                    ? <a href={uri} target="_blank" className="templates__link">
+                        <ArrowUpRightSquare width={32} height={32} />
+                      </a>
+                    : <PageOffcanvas pages={pages} path={path} id={id} />
+            }
+            <DeleteSubmitModal button={DeleteButton} callback={removeHandler} />
         </Col>
     );
 }
