6b772ce2d07b49e6c902c348032cba221fdf8143
artemWebD <artemtoptun@gmail.com>
2025-03-16 13:53:02 +0700

    added delete form

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,10 +1,13 @@
 import ColorThemeForm from "../color-theme-form/ColorThemeForm";
+import DeleteSubmitForm from "../delete-submit-form/DeleteSubmitForm";
 import SelectGenerateTokenForm from "../select-generate-token-form/SelectGenerateTokenForm";
 
 const Settings = () => {
     return (
         <>
             <ColorThemeForm />
+            <br />
+            <DeleteSubmitForm />
             <br />
             <SelectGenerateTokenForm />
         </>
